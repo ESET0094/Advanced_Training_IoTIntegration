a1437ca1710b8c4ba4e7a199361e4a1dec01c535
ESET0094 <sarath.chandra@esyasoft.com>
2025-10-15 10:00:05 +0530

    Added C# program on Loops

--- CSharp_IoT/LoopingArrayTask/Program.cs
+++ CSharp_IoT/LoopingArrayTask/Program.cs
@@ -1,22 +1,117 @@
-﻿namespace LoopingArrayTask
+﻿using System.ComponentModel.Design;
+
+namespace LoopingArrayTask
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            
-            int[] arr1 , arr2 ;
-            Console.WriteLine("Enter Array - 1:");
-            arr1 = getInput(3);
-            Console.WriteLine("Enter Array - 2:");
-            arr2 = getInput(3);
+
+            int[] arr1, arr2;//, arr3, arr4, arr5, arr6 ;
+            //Console.WriteLine("Enter Array - 1:");
+            arr1 = getRandomIntArray(10);
+            //Console.WriteLine("Enter Array - 2:");
+            arr2 = [1, 2, 3,4];
+            /*(arr3,arr4)= splitArray(arr1);
+            (arr5, arr6) = splitArray(arr2);
+            int sum_arr3 = sumofArray(arr3);
+            int sum_arr4 = sumofArray(arr4);
+            int sum_arr5 = sumofArray(arr5);
+            int sum_arr6 = sumofArray(arr6);
             int[] new_array = sumArrays(arr1, arr2);
-            Console.WriteLine($"Sum of Two Arrays ({string.Join(',',arr1)}) & ({string.Join(',', arr2)}) :{string.Join(',', new_array)}");
             int even_Sum = evenSum(new_array);
-            Console.WriteLine($"Sum of even elements in {string.Join(',', new_array)} is {even_Sum}");
-            int sumofdigits = digitSum(even_Sum);
-            Console.WriteLine($"Sum of digits in the given element is {sumofdigits}");
+            int sumofdigits = digitSum(even_Sum);*/
+            
+            Console.WriteLine($"Array {string.Join(',', arr2)}");
+            int[] revArray = reverseArray(arr2);
+            Console.WriteLine($"Reverse Array {string.Join(',', revArray)}");
         }
+
+        public static int[] reverseArray(int[] arr)
+        {
+            if (arr.Length % 2 == 0)
+            {
+                int temp;
+                for (int i = 0; i < arr.Length/2; i++)
+                {
+
+                    (arr[i], arr[arr.Length - i - 1]) = (arr[arr.Length - i - 1], arr[i]);
+                    
+
+                }
+
+            }
+           
+            return arr;
+            
+            
+            }
+        public static (int[], int[]) splitArray(int[] array)
+        {
+            (int len1, int len2) = countelements(array);
+            int[] evenArray = new int[len1];
+            int[] oddArray = new int[len2];
+            int i = 0;
+            while (i < len1)
+            {
+                for (int j = 0; j < array.Length; j++) {
+                    if (array[j] % 2 == 0)
+                    {
+                        evenArray[i] = array[j];
+                        i++;
+                    }
+                } 
+            }
+            i = 0;
+            while (i < len2)
+            {
+                for (int j = 0; j < array.Length; j++)
+                {
+                    if (array[j] % 2!=0)
+                    {
+                        oddArray[i] = array[j];
+                        i++;
+                    }
+                }
+            }
+            Console.WriteLine($"Even Array : [{string.Join(',', evenArray)}], \nOdd Array : [{string.Join(',', oddArray)}] \nfor the given Array [{string.Join(',', array)}");
+            return (evenArray, oddArray);
+
+        }           
+                  
+        public static (int,int) countelements(int[] array)
+        {
+
+            int countEven = 0;
+            int countOdd = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] % 2 == 0)
+                {
+                    countEven += 1;
+                }
+                else
+                {
+                    countOdd += 1;
+
+                }
+            }
+            return (countEven, countOdd);
+            
+        }
+
+        
+        public static int[] getRandomIntArray(int length)
+        {
+            int[] array = new int[length];
+            Random random = new Random();
+            for (int i = 0; i < length; i++)
+            {
+                array[i] = random.Next(11,100);
+            }
+            return array;
+        }
+        
         public static int[] getInput(int len)
         {
             int[] input = new int[len];
@@ -44,6 +139,8 @@
                 }
 
             }
+            Console.WriteLine($"Sum of Two Arrays ({string.Join(',', arr1)}) & ({string.Join(',', arr2)}) :{string.Join(',', sum)}");
+
             return sum;
         }
         public static int evenSum(int[] arr) {
@@ -55,9 +152,21 @@
                     sum += i;
                 }
             }
+            Console.WriteLine($"Sum of even elements in {string.Join(',', arr)} is {sum}");
+
             return sum;
         }
+        public static int sumofArray(int[] arr)
+        {
+            int sum = 0;
+            foreach (var i in arr)
+                sum+=i;
+            Console.WriteLine($"The sum of elements in {string.Join(',', arr)} is {sum}");
+            digitSum(sum);
+            return sum;
 
+
+        }
         public static int digitSum(int element)
         {
             int rem = 0;
@@ -67,6 +176,8 @@
                 sum += rem;
                 element /= 10;
             }
+            Console.WriteLine($"Sum of digits in the given element is {sum}");
+
             return sum;
         }
         
