0507c788dc131f9c98ee60863036886688b6349b
ESET0094 <sarath.chandra@esyasoft.com>
2025-10-22 17:29:18 +0530

    Added Exceptions

---
+++ CSharp_IoT/Exceptions/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace Exceptions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int first;
+            int second;
+            int[] myarray = new int[10];
+            first = Convert.ToInt32(Console.ReadLine());
+            second = Convert.ToInt32(Console.ReadLine());
+            int result;
+            int result1;
+            int result2;
+            try
+            {
+                result = first / second;
+                try
+                {
+                    result1 = first / second;
+
+                }
+                catch (DivideByZeroException ex)
+                {
+                    throw ex;
+
+                    Console.WriteLine("At 1");
+                }
+                try
+                {
+                    result2 = second / first;
+                    
+                }
+                catch (DivideByZeroException ex)
+                {
+                    Console.WriteLine("At 2");
+                    throw ex;
+                }
+
+
+            }
+            catch (Exception e) {
+                throw e;
+                Console.WriteLine("Exception Found"); }
+            finally
+            {
+                Console.WriteLine("Finally");
+            }
+            Console.WriteLine("Program Terminating.....");
+        }
+    }
+}
